6f2fb61134f6644a6d66a0d41954de5bc9c7eeac
ly <364819377@qq.com>
2018-05-21 17:13:48 +0800

    '-'

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lajor@qq.com
+ * Date: 2018/5/21
+ * Time: 16:35
+ */
+require_once __DIR__.'/vendor/autoload.php';
+use Hello\SayHello;
+
+echo SayHello::world();
---
+++ src/SayHello.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lajor@qq.com
+ * Date: 2018/5/21
+ * Time: 16:20
+ */
+namespace Hello;
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'hello world';
+    }
+}
